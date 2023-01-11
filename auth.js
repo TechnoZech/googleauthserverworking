d82e6d638cc910f414a596a12e75f68c418c2604
unknown <sharmadharmesh1998@gmail.com>
2023-01-11 18:36:08 +0530

    auth cb url updated

--- auth.js
+++ auth.js
@@ -8,7 +8,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:5000/auth/google/callback",
+    callbackURL: "https://googleauthserver.onrender.com/auth/google/callback",
     passReqToCallback: true
   },
 
